f9c657b16e2b6c199595c798cfb04c130b888bca
Hasnaa Al-Habahbeh <hasnaahab98@gmail.com>
2021-08-16 17:07:20 +0300

    finished cart styling

--- js/cart.js
+++ js/cart.js
@@ -1,74 +1,74 @@
-const cartItems = JSON.parse(localStorage.getItem('cart')) || []
-table = document.querySelector('tbody')
-let name = ['name1', 'name2', 'name3', 'name4']
-let cartuser = []
+const cartItems = JSON.parse(localStorage.getItem('cart')) || [];
+table = document.querySelector('tbody');
+let name = ['name1', 'name2', 'name3', 'name4'];
+let cartuser = [];
 for (let i = 0; i < cartItems.length + 1; i++) {
-    let cut = cartItems.indexOf(0)
-    if (cut == -1) { break }
-    cartItems.splice(cut, 1)
-    name.splice(cut, 1)
+  let cut = cartItems.indexOf(0);
+  if (cut == -1) { break; }
+  cartItems.splice(cut, 1);
+  name.splice(cut, 1);
 }
 for (let i = 0; i < cartItems.length + 1; i++) {
 
-    let cut = cartItems.indexOf(null)
-    if (cut == -1) { break }
-    cartItems.splice(cut, 1)
-    name.splice(cut, 1)
-    console.log(cartItems)
+  let cut = cartItems.indexOf(null);
+  if (cut == -1) { break; }
+  cartItems.splice(cut, 1);
+  name.splice(cut, 1);
+  console.log(cartItems);
 
 }
 
 
 for (let i = 0; i < cartItems.length; i++) {
 
-    let tr = document.createElement('tr')
-    tr.id = "row" + i
-    table.appendChild(tr)
+  let tr = document.createElement('tr');
+  tr.id = 'row' + i;
+  table.appendChild(tr);
 
-    let td = document.createElement('td')
-    td.textContent = `remove `
-    td.id = "delete" + i
-    tr.appendChild(td)
+  let td = document.createElement('td');
+  td.textContent = 'remove ';
+  td.id = 'delete' + i;
+  tr.appendChild(td);
 
-    let td1 = document.createElement('td')
-    td1.innerText = cartItems[i]
-    tr.appendChild(td1)
+  let td1 = document.createElement('td');
+  td1.innerText = cartItems[i];
+  tr.appendChild(td1);
 
-    let td2 = document.createElement('td')
-    td2.textContent = name[i]
-    tr.appendChild(td2)
-    cartuser.push(cartItems[i], name[i])
-    localStorage.cart2 = JSON.stringify(cartuser)
+  let td2 = document.createElement('td');
+  td2.textContent = name[i];
+  tr.appendChild(td2);
+  cartuser.push(cartItems[i], name[i]);
+  localStorage.cart2 = JSON.stringify(cartuser);
 
 
 }
 cartuser.push(localStorage.getItem('userOfThisSite'));
-localStorage.cart2 = JSON.stringify(cartuser)
+localStorage.cart2 = JSON.stringify(cartuser);
 const ta = document.getElementById('cart');
 ta.addEventListener('click', removeitem);
 
 function removeitem(event) {
-    let target = event.target.innerText
-    if (target == 'remove') {
-        let child = parseInt(event.target.parentElement.childNodes[1].innerText)
-        event.target.parentElement.remove()
-        let place = cartItems.indexOf(child)
-        cartItems.splice(place, 1)
-        cartuser.splice(place, 2)
+  let target = event.target.innerText;
+  if (target == 'remove') {
+    let child = parseInt(event.target.parentElement.childNodes[1].innerText);
+    event.target.parentElement.remove();
+    let place = cartItems.indexOf(child);
+    cartItems.splice(place, 1);
+    cartuser.splice(place, 2);
 
-        localStorage.cart = JSON.stringify(cartItems)
-        localStorage.cart2 = JSON.stringify(cartuser)
+    localStorage.cart = JSON.stringify(cartItems);
+    localStorage.cart2 = JSON.stringify(cartuser);
 
-    }
+  }
 }
 if (cartuser.length == 1) {
-    cartuser.splice(0, 1);
-    localStorage.cart2 = JSON.stringify(cartuser)
+  cartuser.splice(0, 1);
+  localStorage.cart2 = JSON.stringify(cartuser);
 }
 
 function myclear() {
-    location.reload()
-    localStorage.removeItem('cart')
-    localStorage.removeItem('cart2')
+  location.reload();
+  localStorage.removeItem('cart');
+  localStorage.removeItem('cart2');
 
 }
