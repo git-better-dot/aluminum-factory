958447498a3cf472a0d6c40246cd8be0b6f97612
yaseen1998 <yasseenkouthe@eng.hu.edu.jo>
2021-08-18 03:01:52 +0300

    create search

--- js/product.js
+++ js/product.js
@@ -343,4 +343,127 @@ function quentity(event) {
 
 
 }
+//#################################################################
+//#################################################################
+let section = document.querySelector(".main-container");
+let section2 = document.querySelector(".main-container2");
+let div = section.getElementsByTagName("DIV");
+ let  div2 = section2.getElementsByTagName("DIV");
+function filter(){
+    
+  var filterValue, input,i;
+   input = document.getElementById("search");
+   filterValue = input.value.toUpperCase()
+   console.log(filterValue)
+let error = 0
+      for (i = 0 ; i < div.length ; i=i+5){
+        
+        // console.log(div[i])
+        // console.log(div2[i])
+        console.log(filterValue)
+
+          // a = li[i].getElementsByTagName("a")[0];
+          h = div[i].getElementsByTagName("h2")[0]
+          h2 = div2[i].getElementsByTagName("h2")[0]
+          
+
+          if(h.innerHTML.toUpperCase().indexOf(filterValue) > -1 ){
+            console.log(h.innerHTML.toUpperCase().indexOf(filterValue))
+              div[i].style.display = "";
+
+          }
+              
+         
+            else {
+              error++
+              div[i].style.display = "none";
+              
+            }
+            if(h2.innerHTML.toUpperCase().indexOf(filterValue) > -1){
+              div2[i].style.display = "";
+            }
+            else{
+              error++
+              div2[i].style.display = "none";
+
+            }
+
+      }if(error == 8){
+        let p = document.createElement('p')
+        p.innerText =` this item not avalibale`
+        p.id = 'avalibale'
+        section.appendChild(p)
+      }
+  }
+//#################################################################
+//#################################################################
+
+let pricenumber = []
+function search(){
+  for (i = 0 ; i < div.length ; i=i+5){
+    price = div[i].querySelector('p.price')
+    price2 =  div2[i].querySelector('p.price')
+    let pricearr =  price.innerHTML.split(' ')
+    let pricearr2 = price2.innerHTML.split(' ')
+    pricearr2.splice(0,1)
+    pricearr2.splice(1,1)
+    pricearr.splice(0,1)
+    pricearr.splice(1,1)
+    pricenumber.push(Number(pricearr),Number(pricearr2))
+  }
+}
+search()
+let max;
+let min;
+let newfliter ;
+function filterprice(){
+  console.log(pricenumber)
+
+  let rd1 = document.getElementById('rd1')
+  let rd2 = document.getElementById('rd2')
+  if(rd1.checked==true){
+    max = 19;
+    min = 0;
+
+  }
+  if(rd2.checked==true){
+    max = 30;
+    min = 20;
+    
+  }
+  newfliter= pricenumber.filter(function(value){
+    console.log(min)
+    console.log(max)
+    return (value<=max && value>=min);
+  });
+  
+  for (i = 0 ; i < div.length ; i=i+5){
+    console.log('ello')
+    price = div[i].querySelector('p.price')
+    price2 =  div2[i].querySelector('p.price')
+    let pricearr =  price.innerHTML.split(' ')
+    let pricearr2 = price2.innerHTML.split(' ')
+    pricearr2.splice(0,1)
+    pricearr2.splice(1,1)
+    pricearr2 = Number(pricearr2)
+    pricearr.splice(0,1)
+    pricearr.splice(1,1)
+    pricearr = Number(pricearr)
+if(newfliter.includes(pricearr)){
+  console.log(pricearr)
+div[i].style.display = '';
+}
+else{
+  console.log(pricearr)
+
+  div[i].style.display = 'none';
+}
+if(newfliter.includes(pricearr2)){
+  div2[i].style.display = '';
+  }
+  else{
+    div2[i].style.display = 'none';
+  }
+}}
+
 
